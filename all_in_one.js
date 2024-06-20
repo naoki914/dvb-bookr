17f85a5d61edfb148a048a5b3b23eea3de492d6f
Jose Simoes <jose.simooes@gmail.com>
2024-06-20 20:22:54 +0900

    fix: change default values and fix time slot number

--- all_in_one.js
+++ all_in_one.js
@@ -13,16 +13,17 @@ function openTabsWithDelay(urls, delay) {
         var purpose = document.querySelector('#purpose');
         var agree_checkbox = document.querySelector('#agree_use1');
         var apply_button = document.querySelector('#writeBtn');
-        if (member) member.value = 'MemberName';
-        if (team) team.value = 'TeamName';
+        if (member) member.value = '김승윤';
+        if (team) team.value = 'DVB';
         if (team_yn1) team_yn1.checked = true;
         if (users) users.value = '18';
-        if (mobile) mobile.value = '010-1234-5678';
+        if (mobile) mobile.value = '010-2293-2050';
         if (phone) phone.value = '';
         if (type_cd1) type_cd1.checked = true;
-        if (title) title.value = 'EventName';
-        if (purpose) purpose.value = 'PurposeOfUse';
+        if (title) title.value = '배구';
+        if (purpose) purpose.value = '배구';
         if (agree_checkbox) agree_checkbox.checked = true;
+        if (apply_button) apply_button.click();
     })();
     `;
     urls.forEach(function(url, index) {
@@ -62,7 +63,7 @@ function getUrls() {
     const sundays = getSundaysForComingMonth();
     const after = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1);
     const base_date = `${after.getFullYear()}${String(after.getMonth() + 1).padStart(2, '0')}01`;
-    const time_no = '137;9%ED%9A%8C;1400;1450;1|138;10%ED%9A%8C;1500;1550;1|139;11%ED%9A%8C;1600;1650;1|140;12%ED%9A%8C;1700;1750;1';
+    const time_no = '141;9%ED%9A%8C;1400;1450;1|142;10%ED%9A%8C;1500;1550;1|143;11%ED%9A%8C;1600;1650;1|144;12%ED%9A%8C;1700;1750;1';
     
     const encoded_time_no = encodeURIComponent(time_no);
     
